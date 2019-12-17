62baf7abdc8b6436c834f15fdf1780b9dd091393
Xander <alexander.eagle@ssfs.org>
2019-12-17 14:15:28 -0500

    worked on unit 10 project

---
+++ target.py
@@ -0,0 +1,10 @@
+import pygame, sys
+
+
+class Target:
+
+    def __init__(self, main_surface):
+        self.main_surface = main_surface
+
+    def draw_target(self):
+        pygame.draw.circle(main_surface, )
